15b1b92691086cf67784a9ada2eca1b7a73e3880
Joanna Zhou <joanna.zyz@outlook.com>
2020-01-12 17:11:10 -0500

    Setup new classifier code

--- A1/code/a1_classify.py
+++ A1/code/a1_classify.py
@@ -1,51 +1,75 @@
-from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-import numpy as np
 import argparse
-import sys
 import os
+from scipy.stats import ttest_rel
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import SelectKBest
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import SGDClassifier
 
-def accuracy( C ):
+# Added libraries
+import sys
+import numpy as np
+from sklearn.feature_selection import chi2
+
+
+
+def accuracy(C):
     ''' Compute accuracy given Numpy array confusion matrix C. Returns a floating point value '''
-    print ('TODO')
+    print('TODO')
 
-def recall( C ):
+
+def recall(C):
     ''' Compute recall given Numpy array confusion matrix C. Returns a list of floating point values '''
-    print ('TODO')
+    print('TODO')
 
-def precision( C ):
+
+def precision(C):
     ''' Compute precision given Numpy array confusion matrix C. Returns a list of floating point values '''
-    print ('TODO')
-    
+    print('TODO')
 
-def class31(filename):
+
+def class31(output_dir, X_train, X_test, y_train, y_test):
     ''' This function performs experiment 3.1
     
     Parameters
-       filename : string, the name of the npz file from Task 2
-
-    Returns:      
+       output_dir: path of directory to write output to
        X_train: NumPy array, with the selected training features
        X_test: NumPy array, with the selected testing features
        y_train: NumPy array, with the selected training classes
        y_test: NumPy array, with the selected testing classes
+
+    Returns:      
        i: int, the index of the supposed best classifier
     '''
     print('TODO Section 3.1')
 
-    return (X_train, X_test, y_train, y_test,iBest)
+    with open(f"{output_dir}/a1_3.1.txt", "w") as outf:
+        # For each classifier, compute results and write the following output:
+        #     outf.write(f'Results for {classifier_name}:\n')  # Classifier name
+        #     outf.write(f'\tAccuracy: {accuracy:.4f}\n')
+        #     outf.write(f'\tRecall: {[round(item, 4) for item in recall]}\n')
+        #     outf.write(f'\tPrecision: {[round(item, 4) for item in precision]}\n')
+        #     outf.write(f'\tConfusion Matrix: \n{conf_matrix}\n\n')
+        pass
+
+    return iBest
 
 
-def class32(X_train, X_test, y_train, y_test,iBest):
+def class32(output_dir, X_train, X_test, y_train, y_test, iBest):
     ''' This function performs experiment 3.2
     
     Parameters:
+       output_dir: path of directory to write output to
        X_train: NumPy array, with the selected training features
        X_test: NumPy array, with the selected testing features
        y_train: NumPy array, with the selected training classes
        y_test: NumPy array, with the selected testing classes
-       i: int, the index of the supposed best classifier (from task 3.1)  
+       iBest: int, the index of the supposed best classifier (from task 3.1)  
 
     Returns:
        X_1k: numPy array, just 1K rows of X_train
@@ -53,12 +77,20 @@ def class32(X_train, X_test, y_train, y_test,iBest):
    '''
     print('TODO Section 3.2')
 
+    with open(f"{output_dir}/a1_3.2.txt", "w") as outf:
+        # For each number of training examples, compute results and write
+        # the following output:
+        #     outf.write(f'{num_train}: {accuracy:.4f}\n'))
+        pass
+
     return (X_1k, y_1k)
-    
-def class33(X_train, X_test, y_train, y_test, i, X_1k, y_1k):
+
+
+def class33(output_dir, X_train, X_test, y_train, y_test, i, X_1k, y_1k):
     ''' This function performs experiment 3.3
     
     Parameters:
+       output_dir: path of directory to write output to
        X_train: NumPy array, with the selected training features
        X_test: NumPy array, with the selected testing features
        y_train: NumPy array, with the selected training classes
@@ -69,17 +101,50 @@ def class33(X_train, X_test, y_train, y_test, i, X_1k, y_1k):
     '''
     print('TODO Section 3.3')
 
-def class34( filename, i ):
+    with open(f"{output_dir}/a1_3.3.txt", "w") as outf:
+        # Prepare the variables with corresponding names, then uncomment
+        # this, so it writes them to outf.
+
+        # for each number of features k_feat, write the p-values for
+        # that number of features:
+            # outf.write(f'{k_feat} p-values: {[round(pval, 4) for pval in p_values]}')
+
+        # outf.write(f'Accuracy for 1k: {accuracy_1k:.4f}\n')
+        # outf.write(f'Accuracy for full dataset: {accuracy_full:.4f}\n')
+        # outf.write(f'Chosen feature intersection: {feature_intersection}\n')
+        # outf.write(f'Top-5 at higher: {top_5}\n')
+        pass
+
+
+def class34(output_dir, X_train, X_test, y_train, y_test, i):
     ''' This function performs experiment 3.4
     
     Parameters
-       filename : string, the name of the npz file from Task 2
+       output_dir: path of directory to write output to
+       X_train: NumPy array, with the selected training features
+       X_test: NumPy array, with the selected testing features
+       y_train: NumPy array, with the selected training classes
+       y_test: NumPy array, with the selected testing classes
        i: int, the index of the supposed best classifier (from task 3.1)  
         '''
     print('TODO Section 3.4')
-    
+
+    with open(f"{output_dir}/a1_3.2.txt", "w") as outf:
+        # Prepare kfold_accuracies, then uncomment this, so it writes them to outf.
+        # outf.write(f'Kfold Accuracies: {[round(acc, 4) for acc in kfold_accuracies]}\n')
+        # outf.write(f'p-values: {[round(pval, 4) for pval in p_values]}\n')
+        pass
+
+
 if __name__ == "__main__":
-    parser.add_argument("-i", "--input", help="the input npz file from Task 2", required=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input", help="the input npz file from Task 2", required=True)
+    parser.add_argument(
+        "-o", "--output_dir",
+        help="The directory to write a1_3.X.txt files to.",
+        default=os.path.dirname(os.path.dirname(__file__)))
     args = parser.parse_args()
 
+    # TODO: load data and split into train and test.
     # TODO : complete each classification experiment, in sequence.
