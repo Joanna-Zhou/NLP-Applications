5252747147367105f1d5e366e998d39ec3c6aab9
Joanna Zhou <joanna.zyz@outlook.com>
2020-01-13 12:56:51 -0500

    Finished confusion matrix evals

--- A1/code/a1_classify.py
+++ A1/code/a1_classify.py
@@ -17,20 +17,26 @@ import numpy as np
 from sklearn.feature_selection import chi2
 
 
+# (Reference on confusion matrix: https://www.python-course.eu/confusion_matrix.php)
 
 def accuracy(C):
     ''' Compute accuracy given Numpy array confusion matrix C. Returns a floating point value '''
-    print('TODO')
+    correct = C.trace() # diagnol terms are correctly predicted ones
+    total = C.sum()
+    return 0. if total==0. else correct/total
 
 
 def recall(C):
     ''' Compute recall given Numpy array confusion matrix C. Returns a list of floating point values '''
-    print('TODO')
-
+    correct = C.diagonal()
+    total_per_row = np.sum(C, axis=1)
+    return correct / total_per_row
 
 def precision(C):
     ''' Compute precision given Numpy array confusion matrix C. Returns a list of floating point values '''
-    print('TODO')
+    correct = C.diagonal()
+    total_per_col = np.sum(C, axis=0)
+    return correct / total_per_col
 
 
 def class31(output_dir, X_train, X_test, y_train, y_test):
@@ -128,7 +134,7 @@ def class34(output_dir, X_train, X_test, y_train, y_test, i):
        i: int, the index of the supposed best classifier (from task 3.1)  
         '''
     print('TODO Section 3.4')
-
+    
     with open(f"{output_dir}/a1_3.2.txt", "w") as outf:
         # Prepare kfold_accuracies, then uncomment this, so it writes them to outf.
         # outf.write(f'Kfold Accuracies: {[round(acc, 4) for acc in kfold_accuracies]}\n')
@@ -145,6 +151,30 @@ if __name__ == "__main__":
         help="The directory to write a1_3.X.txt files to.",
         default=os.path.dirname(os.path.dirname(__file__)))
     args = parser.parse_args()
+    
+    # print("Testing confusion matrix computations+++++++++")
+    # C_test = np.array(
+    #     [[5825,    1,   49,   23,    7,   46,   30,   12,   21,   26],
+    #     [1, 6654,   48,   25,   10,   32,   19,   62,  111,   10],
+    #     [2,   20, 5561,   69,   13,   10,    2,   45,   18,    2],
+    #     [6,   26,   99, 5786,    5,  111,    1,   41,  110,   79],
+    #     [4,   10,   43,    6, 5533,   32,   11,   53,   34,   79],
+    #     [3,    1,    2,   56,    0, 4954,   23,    0,   12,    5],
+    #     [31,    4,   42,   22,   45,  103, 5806,    3,   34,    3],
+    #     [0,    4,   30,   29,    5,    6,    0, 5817,    2,   28],
+    #     [35,    6,   63,   58,    8,   59,   26,   13, 5394,   24],
+    #     [16,   16,   21,   57,  216,   68,    0,  219,  115, 5693]])
+    # print(recall(C_test).mean())
+    # print("++++++++++++++++++++++++++++++++++++++++++++++")
 
-    # TODO: load data and split into train and test.
-    # TODO : complete each classification experiment, in sequence.
+    # python3 a1_classify.py -i feats_medium.npz -o /classifier_output/
+
+
+    # # TODO: load data and split into train and test.
+    # filename = args.input
+    # (X_train, X_test, y_train, y_test, iBest) = class31(filename)
+
+    # # TODO : complete each classification experiment, in sequence.
+    # X_1k, y_1k = class32(X_train, X_test, y_train, y_test, iBest)
+    # class33(X_train, X_test, y_train, y_test, iBest, X_1k, y_1k)
+    # class34(filename, iBest)
