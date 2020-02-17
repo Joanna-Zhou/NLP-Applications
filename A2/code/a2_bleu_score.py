d4cd33485d27392649a5714feb934fbab8a24872
Joanna Zhou <joanna.zyz@outlook.com>
2020-02-17 09:24:14 -0500

    Finished bleu score and git ignore

---
+++ A2/code/a2_bleu_score.py
@@ -0,0 +1,112 @@
+# Copyright 2020 University of Toronto, all rights reserved
+
+'''Calculate BLEU score for one reference and one hypothesis
+
+You do not need to import anything more than what is here
+'''
+
+from math import exp  # exp(x) gives e^x
+
+
+def grouper(seq, n):
+    '''Extract all n-grams from a sequence
+
+    An n-gram is a contiguous sub-sequence within `seq` of length `n`. This
+    function extracts them (in order) from `seq`.
+
+    Parameters
+    ----------
+    seq : sequence
+        A sequence of words or token ids representing a transcription.
+    n : int
+        The size of sub-sequence to extract.
+
+    Returns
+    -------
+    ngrams : list
+    '''
+    ngrams = []
+    for i in range(len(seq) - n):
+        ngrams.append(seq[i: i + n])
+    return ngrams
+
+def n_gram_precision(reference, candidate, n):
+    '''Calculate the precision for a given order of n-gram
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+    n : int
+        The order of n-gram precision to calculate
+
+    Returns
+    -------
+    p_n : float
+        The n-gram precision. In the case that the candidate has length 0,
+        `p_n` is 0.
+    '''
+    reference_ngrams = grouper(reference, n)
+    candidate_ngrams = grouper(candidate, n)
+    candidate_match = 0
+    candidate_all = len(candidate)
+
+    for c_ngram in candidate_ngrams:
+        if any([cand_ngram in ref_ngram for ref_ngram in references_ngrams]):
+            candidate_match += 1
+
+    return candidate_match / candidate_all
+
+
+def brevity_penalty(reference, candidate):
+    '''Calculate the brevity penalty between a reference and candidate
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+
+    Returns
+    -------
+    BP : float
+        The brevity penalty. In the case that the candidate transcription is
+        of 0 length, `BP` is 0.
+    '''
+    reference_len, candidate_len = len(reference), len(candidate)
+    brevity = reference_len / candidate_len
+
+    return 1 if brevity < 1 else exp(1 - brev)
+
+
+def BLEU_score(reference, hypothesis, n):
+    '''Calculate the BLEU score
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+    n : int
+        The maximum order of n-gram precision to use in the calculations,
+        inclusive. For example, ``n = 2`` implies both unigram and bigram
+        precision will be accounted for, but not trigram.
+
+    Returns
+    -------
+    bleu : float
+        The BLEU score
+    '''
+    # Multiply together all the precision scores
+    prec_1ton = 1
+    for n in range(1, n+1):
+        prec_1ton *= n_gram_precision(reference, hypothesis, n)
+
+    return brevity_penalty(reference, hypothesis) * (prec_1ton ** (1/n))
