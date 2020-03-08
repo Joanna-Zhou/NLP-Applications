4a6636e8f9a931709f378224c70d91dc37ee86f0
Joanna Zhou <joanna.zyz@outlook.com>
2020-03-08 15:27:55 -0400

    Last attempt before i give up on the cuda stuff :(

--- A2/code/a2_bleu_score.py
+++ A2/code/a2_bleu_score.py
@@ -1,40 +1,144 @@
-# Copyright 2020 University of Toronto, all rights reserved
+# # Copyright 2020 University of Toronto, all rights reserved
+
+# '''Calculate BLEU score for one reference and one hypothesis
+
+# You do not need to import anything more than what is here
+# '''
+
+# from math import exp  # exp(x) gives e^x
+
+
+# def grouper(seq, n):
+#     '''Extract all n-grams from a sequence
+
+#     An n-gram is a contiguous sub-sequence within `seq` of length `n`. This
+#     function extracts them (in order) from `seq`.
+
+#     Parameters
+#     ----------
+#     seq : sequence
+#         A sequence of words or token ids representing a transcription.
+#     n : int
+#         The size of sub-sequence to extract.
+
+#     Returns
+#     -------
+#     ngrams : list
+#     '''
+#     ngrams = []
+#     for i in range(len(seq) - n + 1):
+#         ngrams.append(seq[i: i + n])
+
+#     # print("{}-gram generated: {}".format(n, ngrams))
+#     return ngrams
+
+# def n_gram_precision(reference, candidate, n):
+#     '''Calculate the precision for a given order of n-gram
+
+#     Parameters
+#     ----------
+#     reference : sequence
+#         The reference transcription. A sequence of words or token ids.
+#     candidate : sequence
+#         The candidate transcription. A sequence of words or token ids
+#         (whichever is used by `reference`)
+#     n : int
+#         The order of n-gram precision to calculate
+
+#     Returns
+#     -------
+#     p_n : float
+#         The n-gram precision. In the case that the candidate has length 0,
+#         `p_n` is 0.
+#     '''
+#     reference_ngrams = grouper(reference, n)
+#     candidate_ngrams = grouper(candidate, n)
+
+#     candidate_matches = sum(
+#         [1 for cand in candidate_ngrams if cand in reference_ngrams])
+
+#     # print("{}-gram precision: {}/{}".format(n, candidate_match, candidate_all))
+#     return float(candidate_matches / len(candidate_ngrams)) if len(candidate_ngrams) >= n else 0
+
+
+# def brevity_penalty(reference, candidate):
+#     '''Calculate the brevity penalty between a reference and candidate
+
+#     Parameters
+#     ----------
+#     reference : sequence
+#         The reference transcription. A sequence of words or token ids.
+#     candidate : sequence
+#         The candidate transcription. A sequence of words or token ids
+#         (whichever is used by `reference`)
+
+#     Returns
+#     -------
+#     BP : float
+#         The brevity penalty. In the case that the candidate transcription is
+#         of 0 length, `BP` is 0.
+#     '''
+#     reference_len, candidate_len = len(reference), len(candidate)
+#     brevity = reference_len / candidate_len if candidate_len != 0 else 0
+
+#     return 1 if brevity < 1 else exp(1 - brevity)
+
+
+# def BLEU_score(reference, hypothesis, n):
+#     '''Calculate the BLEU score
+
+#     Parameters
+#     ----------
+#     reference : sequence
+#         The reference transcription. A sequence of words or token ids.
+#     candidate : sequence
+#         The candidate transcription. A sequence of words or token ids
+#         (whichever is used by `reference`)
+#     n : int
+#         The maximum order of n-gram precision to use in the calculations,
+#         inclusive. For example, ``n = 2`` implies both unigram and bigram
+#         precision will be accounted for, but not trigram.
+
+#     Returns
+#     -------
+#     bleu : float
+#         The BLEU score
+#     '''
+#     # Multiply together all the precision scores
+#     prec_1ton = 1
+#     for n in range(1, n+1):
+#         prec_1ton *= n_gram_precision(reference, hypothesis, n)
+
+#     return float(brevity_penalty(reference, hypothesis) * (prec_1ton ** (1./n)))
 
 '''Calculate BLEU score for one reference and one hypothesis
-
 You do not need to import anything more than what is here
 '''
 
-from math import exp  # exp(x) gives e^x
+from math import exp, pow  # exp(x) gives e^x
 
 
 def grouper(seq, n):
     '''Extract all n-grams from a sequence
-
     An n-gram is a contiguous sub-sequence within `seq` of length `n`. This
     function extracts them (in order) from `seq`.
-
     Parameters
     ----------
     seq : sequence
         A sequence of words or token ids representing a transcription.
     n : int
         The size of sub-sequence to extract.
-
     Returns
     -------
     ngrams : list
     '''
-    ngrams = []
-    for i in range(len(seq) - n + 1):
-        ngrams.append(seq[i: i + n])
-
-    # print("{}-gram generated: {}".format(n, ngrams))
+    end = len(seq) - n+1
+    ngrams = [seq[i:i+n] for i in range(end)]
     return ngrams
+
 
 def n_gram_precision(reference, candidate, n):
     '''Calculate the precision for a given order of n-gram
-
     Parameters
     ----------
     reference : sequence
@@ -44,26 +148,23 @@ def n_gram_precision(reference, candidate, n):
         (whichever is used by `reference`)
     n : int
         The order of n-gram precision to calculate
-
     Returns
     -------
     p_n : float
         The n-gram precision. In the case that the candidate has length 0,
         `p_n` is 0.
     '''
-    reference_ngrams = grouper(reference, n)
-    candidate_ngrams = grouper(candidate, n)
-
-    candidate_matches = sum(
-        [1 for cand in candidate_ngrams if cand in reference_ngrams])
-
-    # print("{}-gram precision: {}/{}".format(n, candidate_match, candidate_all))
-    return float(candidate_matches / len(candidate_ngrams)) if len(candidate_ngrams) >= n else 0
+    # if n == 0?
+    if len(candidate) < n:
+        return 0
+    refgrams = grouper(reference, n)
+    candgrams = grouper(candidate, n)
+    matches = sum([1 for cand in candgrams if cand in refgrams])
+    return float(matches / len(candgrams))
 
 
 def brevity_penalty(reference, candidate):
     '''Calculate the brevity penalty between a reference and candidate
-
     Parameters
     ----------
     reference : sequence
@@ -71,22 +172,20 @@ def brevity_penalty(reference, candidate):
     candidate : sequence
         The candidate transcription. A sequence of words or token ids
         (whichever is used by `reference`)
-
     Returns
     -------
     BP : float
         The brevity penalty. In the case that the candidate transcription is
         of 0 length, `BP` is 0.
     '''
-    reference_len, candidate_len = len(reference), len(candidate)
-    brevity = reference_len / candidate_len if candidate_len != 0 else 0
+    if len(candidate) == 0:
+        return 0
+    brevity = len(reference) / len(candidate)
+    return 1 if brevity < 1 else float(exp(1-brevity))
 
-    return 1 if brevity < 1 else exp(1 - brevity)
 
-
-def BLEU_score(reference, hypothesis, n):
+def BLEU_score(reference, candidate, n):
     '''Calculate the BLEU score
-
     Parameters
     ----------
     reference : sequence
@@ -98,15 +197,12 @@ def BLEU_score(reference, hypothesis, n):
         The maximum order of n-gram precision to use in the calculations,
         inclusive. For example, ``n = 2`` implies both unigram and bigram
         precision will be accounted for, but not trigram.
-
     Returns
     -------
     bleu : float
         The BLEU score
     '''
-    # Multiply together all the precision scores
-    prec_1ton = 1
-    for n in range(1, n+1):
-        prec_1ton *= n_gram_precision(reference, hypothesis, n)
-
-    return float(brevity_penalty(reference, hypothesis) * (prec_1ton ** (1./n)))
+    res = 1
+    for i in range(1, n+1):
+        res *= n_gram_precision(reference, candidate, i)
+    return float(brevity_penalty(reference, candidate)*pow(res, 1./n))
--- A2/code/a2_training_and_testing.py
+++ A2/code/a2_training_and_testing.py
@@ -1,226 +1,9 @@
-# # Copyright 2020 University of Toronto, all rights reserved
+# Copyright 2020 University of Toronto, all rights reserved
 
-# '''Functions related to training and testing.
+'''Functions related to training and testing.
 
-# You don't need anything more than what's been imported here.
-# '''
-
-# import torch
-# import a2_bleu_score
-
-
-# from tqdm import tqdm
-
-
-# def train_for_epoch(model, dataloader, optimizer, device):
-#     '''Train an EncoderDecoder for an epoch
-
-#     An epoch is one full loop through the training data. This function:
-
-#     1. Defines a loss function using :class:`torch.nn.CrossEntropyLoss`,
-#        keeping track of what id the loss considers "padding"
-#     2. For every iteration of the `dataloader` (which yields triples
-#        ``F, F_lens, E``)
-#        1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
-#           for ``F_lens`` and ``E``.
-#        2. Zeros out the model's previous gradient with ``optimizer.zero_grad()``
-#        3. Calls ``logits = model(F, F_lens, E)`` to determine next-token
-#           probabilities.
-#        4. Modifies ``E`` for the loss function, getting rid of a token and
-#           replacing excess end-of-sequence tokens with padding using
-#         ``model.get_target_padding_mask()`` and ``torch.masked_fill``
-#        5. Flattens out the sequence dimension into the batch dimension of both
-#           ``logits`` and ``E``
-#        6. Calls ``loss = loss_fn(logits, E)`` to calculate the batch loss
-#        7. Calls ``loss.backward()`` to backpropagate gradients through
-#           ``model``
-#        8. Calls ``optim.step()`` to update model parameters
-#     3. Returns the average loss over sequences
-
-#     Parameters
-#     ----------
-#     model : EncoderDecoder
-#         The model we're training.
-#     dataloader : HansardDataLoader
-#         Serves up batches of data.
-#     device : torch.device
-#         A torch device, like 'cpu' or 'cuda'. Where to perform computations.
-#     optimizer : torch.optim.Optimizer
-#         Implements some algorithm for updating parameters using gradient
-#         calculations.
-
-#     Returns
-#     -------
-#     avg_loss : float
-#         The total loss divided by the total numer of sequence
-#     '''
-
-#     # If you are running into CUDA memory errors part way through training,
-#     # try "del F, F_lens, E, logits, loss" at the end of each iteration of
-#     # the loop.
-#     # with torch.no_grad():
-#     # 1. Defines a loss function using :class:`torch.nn.CrossEntropyLoss`,
-#     #    keeping track of what id the loss considers "padding"
-#     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=model.target_eos)
-#     total_loss = 0
-
-#     # 2. For every iteration of the `dataloader` (which yields triples
-#     #    ``F, F_lens, E``)
-#     for F, F_lens, E in dataloader:
-#         # 1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
-#         #       for ``F_lens`` and ``E``.
-#         F = F.to(device)
-#         F_lens = F_lens.to(device)
-#         E = E.to(device)
-
-#         # 2. Zeros out the model's previous gradient with ``optimizer.zero_grad()``
-#         optimizer.zero_grad()
-
-#         # 3. Calls ``logits = model(F, F_lens, E)`` to determine next-token
-#         #     probabilities.
-#         logits = model(F, F_lens, E, 'ignore')
-#         #TODO: print("logits: {},".format(logits.shape)) should return (16, 100, 20000) instead of (100, 320000)
-
-#         # 4. Modifies ``E`` for the loss function, getting rid of a token and
-#         #     replacing excess end-of-sequence tokens with padding using
-#         # ``model.get_target_padding_mask()`` and ``torch.masked_fill``
-#         pad_mask = model.get_target_padding_mask(E)
-#         E = E.masked_fill(pad_mask, model.target_eos)
-
-#         # 5. Flattens out the sequence dimension into the batch dimension of both
-#         #     ``logits`` and ``E``
-#         logits_flat = logits.view(-1, logits.size(-1)) # (T - 1, N, V) -> ((T-1)*N, V)
-#         # print("T: {}, N: {}, V: {}".format(E.shape[0], E.shape[1], model.target_vocab_size))
-#         E = E.transpose(0, 1)[:, 1:].reshape(-1)
-#         # print("E: {}, logits_flat: {}".format(E.shape, logits_flat.shape))
-
-#         # 6. Calls ``loss = loss_fn(logits, E)`` to calculate the batch loss
-#         loss = loss_fn(logits_flat, E)
-#         total_loss += loss
-
-#         # 7. Calls ``loss.backward()`` to backpropagate gradients through
-#         #     ``model``
-#         loss.backward()
-
-#         # 8. Calls ``optim.step()`` to update model parameters
-#         optimizer.step()
-
-#         # To prevent CUDA memory errors part way through training
-#         del F, F_lens, E, logits, loss
-
-#     # 3. Returns the average loss over sequences
-#     avg_bleu = total_loss / len(dataloader.data)
-#     return avg_bleu
-
-
-# def compute_batch_total_bleu(E_ref, E_cand, target_sos, target_eos):
-#     '''Compute the total BLEU score over elements in a batch
-
-#     Parameters
-#     ----------
-#     E_ref : torch.LongTensor
-#         A batch of reference transcripts of shape ``(T, N)``, including
-#         start-of-sequence tags and right-padded with end-of-sequence tags.
-#     E_cand : torch.LongTensor
-#         A batch of candidate transcripts of shape ``(T', N)``, also including
-#         start-of-sequence and end-of-sequence tags.
-#     target_sos : int
-#         The ID of the start-of-sequence tag in the target vocabulary.
-#     target_eos : int
-#         The ID of the end-of-sequence tag in the target vocabulary.
-
-#     Returns
-#     -------
-#     total_bleu : float
-#         The sum total BLEU score for across all elements in the batch. Use
-#         n-gram precision 4.
-#     '''
-#     score, ngram, N = 0, 4, E_ref.shape[1]
-#     refs, cands = E_ref.tolist(), E_cand.tolist()
-
-#     for ref, cand in zip(refs, cands):
-#         ref = ref.squeeze(0)
-#         ref = ref[((ref != target_sos) & (ref != target_eos)
-#                    ).nonzero().squeeze()].tolist()
-#         if not isinstance(ref, list):
-#             ref = [ref]
-
-#         cand = cand.squeeze(0)
-#         cand = cand[((cand != target_sos) & (cand != target_eos)
-#                      ).nonzero().squeeze()].tolist()
-#         if not isinstance(cand, list):
-#             cand = [cand]
-
-#         score += a2_bleu_score.BLEU_score(ref, cand, ngram)
-#     return score
-
-
-# def compute_average_bleu_over_dataset(
-#         model, dataloader, target_sos, target_eos, device):
-#     '''Determine the average BLEU score across sequences
-
-#     This function computes the average BLEU score across all sequences in
-#     a single loop through the `dataloader`.
-
-#     1. For every iteration of the `dataloader` (which yields triples
-#        ``F, F_lens, E_ref``):
-#        1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
-#           for ``F_lens``. No need for ``E_cand``, since it will always be
-#           compared on the CPU.
-#        2. Performs a beam search by calling ``b_1 = model(F, F_lens)``
-#        3. Extracts the top path per beam as ``E_cand = b_1[..., 0]``
-#        4. Computes the total BLEU score of the batch using
-#           :func:`compute_batch_total_bleu`
-#     2. Returns the average per-sequence BLEU score
-
-#     Parameters
-#     ----------
-#     model : EncoderDecoder
-#         The model we're testing.
-#     dataloader : HansardDataLoader
-#         Serves up batches of data.
-#     target_sos : int
-#         The ID of the start-of-sequence tag in the target vocabulary.
-#     target_eos : int
-#         The ID of the end-of-sequence tag in the target vocabulary.
-
-#     Returns
-#     -------
-#     avg_bleu : float
-#         The total BLEU score summed over all sequences divided by the number of
-#         sequences
-#     '''
-
-#     with torch.no_grad():
-#         total_score, total_batches = 0, 0
-
-#         # For every iteration of the `dataloader` (which yields triples``F, F_lens, E_ref``):
-#         for F, F_lens, E_ref in tqdm(dataloader):
-#             # 1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
-#             # for ``F_lens``. No need for ``E_cand``, since it will always be
-#             # compared on the CPU.
-#             batch_size = F.shape[1]
-#             F, F_lens = F.to(device), F_lens.to(device)
-
-#             # 2. Performs a beam search by calling ``b_1 = model(F, F_lens)``
-#             # shape (T', N, self.beam_width)
-#             b_1 = model(F, F_lens)
-
-#             # 3. Extracts the top path per beam as ``E_cand = b_1[..., 0]`` of shape (T', N)
-#             E_cand = b_1[:, :, 0]
-
-#             # 4. Computes the total BLEU score of the batch using func: `compute_batch_total_bleu`
-#             total_score += compute_batch_total_bleu(
-#                 E_ref, E_cand, target_sos, target_eos)
-#             total_batches += 1
-
-#             del F, F_lens, E_cand, b_1, E_ref
-
-#     # Returns the average per-sequence BLEU score
-#     avg_bleu = total_score / \
-#         len(dataloader.data) if len(dataloader.data) != 0 else 0
-
-#     return avg_bleu
+You don't need anything more than what's been imported here.
+'''
 
 import torch
 import a2_bleu_score
@@ -231,7 +14,9 @@ from tqdm import tqdm
 
 def train_for_epoch(model, dataloader, optimizer, device):
     '''Train an EncoderDecoder for an epoch
+
     An epoch is one full loop through the training data. This function:
+
     1. Defines a loss function using :class:`torch.nn.CrossEntropyLoss`,
        keeping track of what id the loss considers "padding"
     2. For every iteration of the `dataloader` (which yields triples
@@ -251,6 +36,7 @@ def train_for_epoch(model, dataloader, optimizer, device):
           ``model``
        8. Calls ``optim.step()`` to update model parameters
     3. Returns the average loss over sequences
+
     Parameters
     ----------
     model : EncoderDecoder
@@ -262,55 +48,74 @@ def train_for_epoch(model, dataloader, optimizer, device):
     optimizer : torch.optim.Optimizer
         Implements some algorithm for updating parameters using gradient
         calculations.
+
     Returns
     -------
     avg_loss : float
-        The total loss divided by the total number of sequence
+        The total loss divided by the total numer of sequence
     '''
-    # If you want, instead of looping through your dataloader as
-    # for ... in dataloader: ...
-    # you can wrap dataloader with "tqdm":
-    # for ... in tqdm(dataloader): ...
-    # This will update a progress bar on every iteration that it prints
-    # to stdout. It's a good gauge for how long the rest of the epoch
-    # will take. This is entirely optional - we won't grade you differently
-    # either way.
+
     # If you are running into CUDA memory errors part way through training,
     # try "del F, F_lens, E, logits, loss" at the end of each iteration of
     # the loop.
-    print('train for epoch')
+    # with torch.no_grad():
+    # 1. Defines a loss function using :class:`torch.nn.CrossEntropyLoss`,
+    #    keeping track of what id the loss considers "padding"
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=model.target_eos)
     total_loss = 0
-    n_batches = 0
+
+    # 2. For every iteration of the `dataloader` (which yields triples
+    #    ``F, F_lens, E``)
     for F, F_lens, E in dataloader:
-        # send to appropriate devices
+        # 1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
+        #       for ``F_lens`` and ``E``.
         F = F.to(device)
         F_lens = F_lens.to(device)
         E = E.to(device)
-        # zero the gradient
-        optimizer.zero_grad()
-        # get logits
-        logits = model(F, F_lens, E, 'ignore')
-        mask = model.get_target_padding_mask(E)
-        E = E.masked_fill(mask, model.target_eos)
-        # 5. Flatten sequence dimension
-        logits = logits.view(-1, logits.size()[-1])  # (T-1, N, V) -> ((T-1)*N, V)
-        E = E.transpose(0, 1)
-        E = E[:, 1:].reshape(-1)  # target,  (N, T) -> ((T-1)*N, 1)
-        loss = loss_fn(logits, E)  # T-1
-        total_loss += loss
-        loss.backward()
-        optimizer.step()
-        n_batches += 1
 
+        # 2. Zeros out the model's previous gradient with ``optimizer.zero_grad()``
+        optimizer.zero_grad()
+
+        # 3. Calls ``logits = model(F, F_lens, E)`` to determine next-token
+        #     probabilities.
+        logits = model(F, F_lens, E, 'ignore')
+        #TODO: print("logits: {},".format(logits.shape)) should return (16, 100, 20000) instead of (100, 320000)
+
+        # 4. Modifies ``E`` for the loss function, getting rid of a token and
+        #     replacing excess end-of-sequence tokens with padding using
+        # ``model.get_target_padding_mask()`` and ``torch.masked_fill``
+        pad_mask = model.get_target_padding_mask(E)
+        E = E.masked_fill(pad_mask, model.target_eos)
+
+        # 5. Flattens out the sequence dimension into the batch dimension of both
+        #     ``logits`` and ``E``
+        logits_flat = logits.view(-1, logits.size(-1)) # (T - 1, N, V) -> ((T-1)*N, V)
+        # print("T: {}, N: {}, V: {}".format(E.shape[0], E.shape[1], model.target_vocab_size))
+        E = E.transpose(0, 1)[:, 1:].reshape(-1)
+        # print("E: {}, logits_flat: {}".format(E.shape, logits_flat.shape))
+
+        # 6. Calls ``loss = loss_fn(logits, E)`` to calculate the batch loss
+        loss = loss_fn(logits_flat, E)
+        total_loss += loss
+
+        # 7. Calls ``loss.backward()`` to backpropagate gradients through
+        #     ``model``
+        loss.backward()
+
+        # 8. Calls ``optim.step()`` to update model parameters
+        optimizer.step()
+
+        # To prevent CUDA memory errors part way through training
         del F, F_lens, E, logits, loss
-    print(f'loss={total_loss/n_batches}')
-    print(f'n_batches: {n_batches}')
-    return total_loss/n_batches
+
+    # 3. Returns the average loss over sequences
+    avg_bleu = total_loss / len(dataloader.data)
+    return avg_bleu
 
 
 def compute_batch_total_bleu(E_ref, E_cand, target_sos, target_eos):
     '''Compute the total BLEU score over elements in a batch
+
     Parameters
     ----------
     E_ref : torch.LongTensor
@@ -323,27 +128,40 @@ def compute_batch_total_bleu(E_ref, E_cand, target_sos, target_eos):
         The ID of the start-of-sequence tag in the target vocabulary.
     target_eos : int
         The ID of the end-of-sequence tag in the target vocabulary.
+
     Returns
     -------
     total_bleu : float
         The sum total BLEU score for across all elements in the batch. Use
         n-gram precision 4.
     '''
-    # you can use E_ref.tolist() to convert the LongTensor to a python list
-    # of numbers
-    total = 0
-    for ref, cand in zip(E_ref.tolist(), E_cand.tolist()):
-        ref = " ".join([str(r) for r in ref]).replace(str(target_eos), '').replace(str(target_sos), '').strip().split(' ')
-        cand = " ".join([str(c) for c in cand]).replace(str(target_eos), '').replace(str(target_sos), '').strip().split(' ')
-        total += a2_bleu_score.BLEU_score(ref, cand, 4)
-    return total
+    score, ngram, N = 0, 4, E_ref.shape[1]
+    refs, cands = E_ref.tolist(), E_cand.tolist()
+
+    for ref, cand in zip(refs, cands):
+        ref = ref.squeeze(0)
+        ref = ref[((ref != target_sos) & (ref != target_eos)
+                   ).nonzero().squeeze()].tolist()
+        if not isinstance(ref, list):
+            ref = [ref]
+
+        cand = cand.squeeze(0)
+        cand = cand[((cand != target_sos) & (cand != target_eos)
+                     ).nonzero().squeeze()].tolist()
+        if not isinstance(cand, list):
+            cand = [cand]
+
+        score += a2_bleu_score.BLEU_score(ref, cand, ngram)
+    return score
 
 
 def compute_average_bleu_over_dataset(
         model, dataloader, target_sos, target_eos, device):
     '''Determine the average BLEU score across sequences
+
     This function computes the average BLEU score across all sequences in
     a single loop through the `dataloader`.
+
     1. For every iteration of the `dataloader` (which yields triples
        ``F, F_lens, E_ref``):
        1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
@@ -354,6 +172,7 @@ def compute_average_bleu_over_dataset(
        4. Computes the total BLEU score of the batch using
           :func:`compute_batch_total_bleu`
     2. Returns the average per-sequence BLEU score
+
     Parameters
     ----------
     model : EncoderDecoder
@@ -364,20 +183,41 @@ def compute_average_bleu_over_dataset(
         The ID of the start-of-sequence tag in the target vocabulary.
     target_eos : int
         The ID of the end-of-sequence tag in the target vocabulary.
+
     Returns
     -------
     avg_bleu : float
         The total BLEU score summed over all sequences divided by the number of
         sequences
     '''
-    n_batch = 0
-    total = 0
-    print('evaluating')
-    for F, F_lens, E in tqdm(dataloader):
-        F = F.to(device)
-        F_lens = F_lens.to(device)
-        b_1 = model(F, F_lens)
-        E_cand = b_1[:, 0]
-        total += compute_batch_total_bleu(E, E_cand, target_sos, target_eos)
-        n_batch += 1
-    return total / n_batch
+
+    with torch.no_grad():
+        total_score, total_batches = 0, 0
+
+        # For every iteration of the `dataloader` (which yields triples``F, F_lens, E_ref``):
+        for F, F_lens, E_ref in tqdm(dataloader):
+            # 1. Sends ``F`` to the appropriate device via ``F = F.to(device)``. Same
+            # for ``F_lens``. No need for ``E_cand``, since it will always be
+            # compared on the CPU.
+            batch_size = F.shape[1]
+            F, F_lens = F.to(device), F_lens.to(device)
+
+            # 2. Performs a beam search by calling ``b_1 = model(F, F_lens)``
+            # shape (T', N, self.beam_width)
+            b_1 = model(F, F_lens)
+
+            # 3. Extracts the top path per beam as ``E_cand = b_1[..., 0]`` of shape (T', N)
+            E_cand = b_1[:, :, 0]
+
+            # 4. Computes the total BLEU score of the batch using func: `compute_batch_total_bleu`
+            total_score += compute_batch_total_bleu(
+                E_ref, E_cand, target_sos, target_eos)
+            total_batches += 1
+
+            del F, F_lens, E_cand, b_1, E_ref
+
+    # Returns the average per-sequence BLEU score
+    avg_bleu = total_score / \
+        len(dataloader.data) if len(dataloader.data) != 0 else 0
+
+    return avg_bleu
